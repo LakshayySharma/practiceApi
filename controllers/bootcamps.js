a3c3421cf47ba78e60dea29abbb12268b34a15f6
Lakshay <ls199575@gmail.com>
2020-04-11 11:07:47 +0530

    basic controller setup

---
+++ controllers/bootcamps.js
@@ -0,0 +1,16 @@
+exports.getBootcamps = (req, res, next) => {
+    res.status(200).json({success:true});
+}
+
+exports.getBootcamp = (req, res, next) => {
+    res.status(200).json({success:true});
+}
+exports.createBootcamp = (req, res, next) => {
+    res.status(200).json({success:true});
+}
+exports.updateBootcamp = (req, res, next) => {
+    res.status(200).json({success:true});
+}
+exports.deleteBootcamp = (req, res, next) => {
+    res.status(200).json({success:true});
+}
