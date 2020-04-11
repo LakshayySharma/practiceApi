5dbd6ebb3d40ffc23190690c94ed76490e6e63ed
Lakshay <ls199575@gmail.com>
2020-04-11 11:06:03 +0530

    add route

--- server.js
+++ server.js
@@ -1,11 +1,22 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+
+
+const bootcamps = require('./routes/bootcamps');
 
 dotenv.config({ path: './config/config.env'});
 
 const app = express();
 
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'));
+}
+
+
+app.use('/api/v1/bootcamps', bootcamps);
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+    console.log(`Server running on port ${PORT}`);
 });
