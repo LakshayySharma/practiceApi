29f57ebcf414a750654b308aaa47bea43ba7a2e1
Lakshay <ls199575@gmail.com>
2020-04-11 15:06:29 +0530

    connect error handler middleware

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
 const connectDb = require('./config/db');
-
+const errorHandler = require('./middleware/error');
 dotenv.config({ path: './config/config.env'});
 
 connectDb();
@@ -19,6 +19,7 @@ if(process.env.NODE_ENV === 'development') {
 
 app.use('/api/v1/bootcamps', bootcamps);
 
+app.use(errorHandler);
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
