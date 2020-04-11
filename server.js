feac6ad6dab3f83b56ea485d1132dc36d5933c34
Lakshay <ls199575@gmail.com>
2020-04-11 14:39:52 +0530

    body parser added

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const morgan = require('morgan');
-
-
-const bootcamps = require('./routes/bootcamps');
+const connectDb = require('./config/db');
 
 dotenv.config({ path: './config/config.env'});
 
+connectDb();
+const bootcamps = require('./routes/bootcamps');
+
 const app = express();
+
+app.use(express.json());
 
 if(process.env.NODE_ENV === 'development') {
     app.use(morgan('dev'));
